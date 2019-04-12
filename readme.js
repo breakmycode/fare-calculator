73cc05d43b2a2e4469ddf8fa0a8dcc46dcafc8f0
breakmycode <vaibhavkargeti@gmail.com>
2019-04-12 16:44:15 +0530

    initial commit

---
+++ readme.js
@@ -0,0 +1,22 @@
+/*   
+ 
+we need to create test cases in node>js
+--> Testing framework 
+-- assertion
+
+assert keyword :
+
+assert 10=10   --> true alwys 
+
+
+assertion , we match some values, and if that value is false , we can assert it 
+
+
+
+
+
+
+
+
+
+*/
